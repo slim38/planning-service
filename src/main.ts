64288894cd593d8d86d96e288e6ec7fc3cdec3fb
slim38 <93978181+slim38@users.noreply.github.com>
2022-04-24 13:01:28 +0200

    enable cors changed

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,10 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { cors } from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cors())
+  app.enableCors();
   await app.listen(4000);
 }
 
