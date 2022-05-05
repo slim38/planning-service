4a16e81582a8a5224dfa5f0b4d629c66eee34691
slim38 <93978181+slim38@users.noreply.github.com>
2022-05-05 16:09:11 +0200

    fix planned stock

--- src/disposition/disposition.service.ts
+++ src/disposition/disposition.service.ts
@@ -16,6 +16,8 @@ export class DispositionService {
         private articleService: ArticleService
     ) {}
 
+    plannedStockDefault = 50;
+
     async initialize(period: number, articleId: number, salesOrderCount: number) {
         const articles = await this.articleService.findById(articleId, period);
         const article = articles[0];
@@ -29,14 +31,14 @@ export class DispositionService {
         const currentStock = article.amount;
         const waitingListOrderStock = article.waitingList.length > 0 ? article.waitingList[0].amount : 0;
         const ordersInWorkCount = article.ordersInWork.length > 0 ? article.ordersInWork[0].amount : 0;
-        const productionOrderCount = salesOrderCount - waitingListOrderStock - ordersInWorkCount;
+        const productionOrderCount = salesOrderCount + this.plannedStockDefault - waitingListOrderStock - ordersInWorkCount;
 
         await this.model.create({
             id,
             period,
             salesArticleId: article.id,
             salesOrderCount,
-            plannedStock: 0,
+            plannedStock: this.plannedStockDefault,
             currentStock,
             waitingListOrderStock,
             ordersInWorkCount,
@@ -51,7 +53,7 @@ export class DispositionService {
                     const currentStock = child.amount;
                     const waitingListOrderStock = child.waitingList.length > 0 ? child.waitingList[0].amount : 0; //TODO: correct calculation
                     const ordersInWorkCount = child.ordersInWork.length > 0 ? child.ordersInWork[0].amount : 0;
-                    const productionOrderCount = salesOrderCount - waitingListOrderStock - ordersInWorkCount;
+                    const productionOrderCount = salesOrderCount + this.plannedStockDefault - waitingListOrderStock - ordersInWorkCount - currentStock;
                     
                     const fieldId: string = v4();
 
