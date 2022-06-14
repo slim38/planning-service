8235d43761d70a6b9032ab957c9dce152d22f7a1
slim38 <93978181+slim38@users.noreply.github.com>
2022-06-14 10:51:52 +0200

    get capacity

--- src/disposition/disposition.controller.ts
+++ src/disposition/disposition.controller.ts
@@ -1,16 +1,20 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { CapacityPlanningService } from 'src/capacity-planning/capacity-planning.service';
 import { Disposition } from './disposition.model';
 import { DispositionService } from './disposition.service';
 
 @Controller('disposition')
 export class DispositionController {
     constructor(
-        private readonly service: DispositionService
+        private readonly service: DispositionService,
+        private readonly capacityService: CapacityPlanningService,
     ) {}
 
     @Post()
     async update(@Body() dispositions) {
         console.log(JSON.stringify(dispositions));
         await this.service.update(dispositions);
+        const period = dispositions.periode;
+        return await this.capacityService.getPlanning(period);
     }
 }
